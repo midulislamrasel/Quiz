36f5796a68bb1cd36b6f028b96b726434d1cb832
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-20 22:50:26 +0600

    add

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat earum reprehenderit molestias aut doloribus nobis? Assumenda odio excepturi tenetur voluptatum nobis possimus esse est quia, asperiores maxime culpa quod aut maiores vero reprehenderit ullam. Animi porro, eaque neque suscipit eius harum nam veniam officiis exercitationem odit, quaerat dolorem. Iure, nesciunt!</p>
         </div>
     );
 };
