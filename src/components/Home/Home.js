31ce2891d9b49f07c708d2c94c44c253ced8ec52
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-20 22:27:42 +0600

    add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <div>
-                
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores corporis architecto tempore repellat aliquid ut totam molestias! Asperiores, aliquid itaque quam beatae repudiandae exercitationem natus, excepturi vitae quaerat maiores consectetur fugit magnam illo earum saepe. Harum dolores quis repellat error porro voluptatibus? Cum earum dolorem officiis dolor consequuntur tenetur rerum?</h2>
             </div>
         </div>
     );
