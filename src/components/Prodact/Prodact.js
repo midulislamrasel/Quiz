4a2238f7c5f645b3bd2221caa9374986d22f48e4
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-21 21:03:02 +0600

    loader

--- src/components/Prodact/Prodact.js
+++ src/components/Prodact/Prodact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Prodact.css'
 
 const Prodact = ({user}) => {
     const { id, name, username } = user;
     return (
-        <div>
+        <div className='user'>
             <h3>Prodact js</h3>
             <h3>ID {id}</h3>
             <h2>Name: {name}</h2>
